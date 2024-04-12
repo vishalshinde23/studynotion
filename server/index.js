f3ca915a771085c74f3b0305346dc7b31e6d94e8
vishal <vishalms2342@gmail.com>
2024-04-12 12:59:20 +0530

    my changes

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "https://studynotion-zeta.vercel.app/",
+		origin: "https://studynotion-zeta.vercel.app",
 		credentials: true,
 	})
 );
